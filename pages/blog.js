32f986783e71e70a9e0c57cda0c2b0e35daa440c
Rich Kevan <rskevan@me.com>
2021-11-12 09:52:05 -0800

    added blog page

---
+++ pages/blog.js
@@ -0,0 +1,29 @@
+import { DataStore } from "aws-amplify";
+import { Blogpost } from "../models";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+const BlogPage = () => {
+    const [blogs, setBlogs] = useState([]);
+    
+    useEffect(() => {
+        DataStore.query(Blogpost).then(data => setBlogs(data));
+    }, []);
+    
+    return (
+        <div>
+        <h1>Blog</h1>
+        <ul>
+            {blogs.map(blog => (
+            <li key={blog.id}>
+                <Link href={`/blog/${blog.id}`}>
+                <a>{blog.title}</a>
+                </Link>
+            </li>
+            ))}
+        </ul>
+        </div>
+    );
+}
+
+export default BlogPage;
