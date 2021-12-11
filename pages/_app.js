006dc345ea318b23cff79989271ded63d7af0dca
rich kevan <rskevan@me.com>
2021-12-10 17:20:39 -0800

    updating meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,10 @@ function MyApp({ Component, pageProps }) {
     <>
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property='og:title' content='Richkevan.com' />
+      <meta name='title' content='Richkevan.com' />
       <meta name="description" content="Rich Kevan's Portfolio" />
-      <meta property='og:image' content='../public/shareimg.png' />
-      <meta property='og:url' content='https://richkevan.com' />
+      <meta name='thumbnail' content='../public/shareimg.png' />
+      <meta name='url' content='https://richkevan.com' />
     </Head>
     <Layout>
       <Component {...pageProps} />
