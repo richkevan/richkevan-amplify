991f4c336725f9a1fc18daf8308a8deb41b759d4
rich kevan <rskevan@me.com>
2021-12-10 19:29:37 -0800

    public folder test

--- components/Meta/meta.js
+++ components/Meta/meta.js
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import thumbnail from '../../public/shareimg.png'
 
 const PageMeta = () => {
 
@@ -9,7 +8,7 @@ const PageMeta = () => {
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta property='og:title' content='Richkevan.com' />
             <meta property="og:description" content="Rich Kevan's Portfolio" />
-            <meta property='og:image' content={thumbnail.src} key='ogimage' />
+            <meta property='og:image' content='/shareimg.png' key='ogimage' />
             <meta property='og:url' content='https://richkevan.com' />
         </Head>
     )
