618f28e92d2acdaf4cc17808e17e299dbeb3788b
rich kevan <rskevan@me.com>
2021-12-10 16:35:20 -0800

    added head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import Layout from '../components/layout'
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
@@ -9,9 +10,17 @@ Amplify.configure({
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>Rich Kevan</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="Rich Kevan's Portfolio" />
+      <meta property='og:image' content='../public/shareimg.png' />
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
