3a73900576f195d82752a25fe8f72433c4d940dd
rich kevan <rskevan@me.com>
2021-12-10 18:57:11 -0800

    changed meta to component

---
+++ components/Meta/meta.js
@@ -0,0 +1,18 @@
+import Head from "next/head"
+import thumbnail from '../public/shareimg.png'
+
+const PageMeta = () => {
+
+
+    return (
+        <Head>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta property='og:title' content='Richkevan.com' />
+            <meta property="og:description" content="Rich Kevan's Portfolio" />
+            <meta property='og:image' content={thumbnail} key='ogimage' />
+            <meta property='og:url' content='https://richkevan.com' />
+        </Head>
+    )
+}
+
+export default PageMeta
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import HeroSlide from '../components/Hero_component/HeroSlide'
+import PageMeta from '../components/Meta/meta'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <PageMeta />
       <HeroSlide />
     </div>
   )
