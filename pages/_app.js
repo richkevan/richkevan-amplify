4000f34f782d754c98dd76937fd356652b79c9bc
rich kevan <rskevan@me.com>
2021-12-10 16:39:22 -0800

    update heat and meta

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-      <title>Rich Kevan</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property='og:title' content='Richkevan.com' />
       <meta name="description" content="Rich Kevan's Portfolio" />
       <meta property='og:image' content='../public/shareimg.png' />
+      <meta property='og:url' content='https://richkevan.com' />
     </Head>
     <Layout>
       <Component {...pageProps} />
