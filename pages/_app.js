068896757a6c7e776dc5a59a9d158aa0305a223b
rich kevan <rskevan@me.com>
2021-12-10 17:43:28 -0800

    thumbnail test

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import Head from 'next/head'
 import Layout from '../components/layout'
 import { motion } from 'framer-motion'
+import thumbnail from '../public/shareimg.png'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,7 +11,7 @@ function MyApp({ Component, pageProps }) {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name='title' content='Richkevan.com' />
       <meta name="description" content="Rich Kevan's Portfolio" />
-      <meta name='thumbnail' content='../public/shareimg.png' />
+      <meta name='thumbnail' content={thumbnail} />
       <meta name='url' content='https://richkevan.com' />
     </Head>
     <Layout>
