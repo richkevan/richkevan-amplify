7ec3495487511861cd12cade3084ec896ff8d5d1
rich kevan <rskevan@me.com>
2021-12-15 17:58:10 -0800

    meta image test

--- components/Meta/meta.js
+++ components/Meta/meta.js
@@ -6,9 +6,10 @@ const PageMeta = () => {
     return (
         <Head>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta property="og:type" content="website"/>
             <meta property='og:title' content='Richkevan.com' />
             <meta property="og:description" content="Rich Kevan's Portfolio" />
-            <meta property='og:image' content='/shareimg.png' key='ogimage' />
+            <meta property='og:image' content='../../public/shareimg.png' key='ogimage' />
             <meta property='og:url' content='https://richkevan.com' />
         </Head>
     )
