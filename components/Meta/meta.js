f708bceb9ee64e0f7c786396fc57db9decc079a6
rich kevan <rskevan@me.com>
2021-12-15 18:44:09 -0800

    using meta generator

--- components/Meta/meta.js
+++ components/Meta/meta.js
@@ -1,16 +1,25 @@
 import Head from "next/head"
 
 const PageMeta = () => {
-
-
     return (
         <Head>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta property="og:type" content="website" />
+            <title>Rich Kevan React/Django Developer</title>
+            <meta name="title" content="Rich Kevan React/Django Developer"/>
+            <meta name="description" content="Cutting Edge Full Stack Engineer • React/Django Developer • Raspberry Pi/Arduino nerd • Robotics fan"/>
+
             <meta property="og:type" content="website"/>
-            <meta property='og:title' content='Rich Kevan Full Stack Developer' />
-            <meta property="og:description" content="Rich Kevan's Portfolio" />
-            <meta property='og:image' content='https://f002.backblazeb2.com/file/portfolio-assets/My+Post+(5).png' key='ogimage' />
-            <meta property='og:url' content='https://richkevan.com' />
+            <meta property="og:url" content="https://richkevan.com/"/>
+            <meta property="og:title" content="Rich Kevan React/Django Developer"/>
+            <meta property="og:description" content="Cutting Edge Full Stack Engineer • React/Django Developer • Raspberry Pi/Arduino nerd • Robotics fan"/>
+            <meta property="og:image" content="https://f002.backblazeb2.com/file/portfolio-assets/metaimage.png"/>
+
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://richkevan.com/"/>
+            <meta property="twitter:title" content="Rich Kevan React/Django Developer"/>
+            <meta property="twitter:description" content="Cutting Edge Full Stack Engineer • React/Django Developer • Raspberry Pi/Arduino nerd • Robotics fan"/>
+            <meta property="twitter:image" content="https://f002.backblazeb2.com/file/portfolio-assets/metaimage.png"/>
         </Head>
     )
 }
