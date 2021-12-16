589b850655b6e78e86f3ca007cdf897078975765
rich kevan <rskevan@me.com>
2021-12-15 18:15:47 -0800

    trying public bucket url

--- components/Meta/meta.js
+++ components/Meta/meta.js
@@ -7,9 +7,9 @@ const PageMeta = () => {
         <Head>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta property="og:type" content="website"/>
-            <meta property='og:title' content='Richkevan.com' />
+            <meta property='og:title' content='Rich Kevan Full Stack Developer' />
             <meta property="og:description" content="Rich Kevan's Portfolio" />
-            <meta property='og:image' content='../../public/shareimg.png' key='ogimage' />
+            <meta property='og:image' content='https://f002.backblazeb2.com/file/portfolio-assets/My+Post+(5).png' key='ogimage' />
             <meta property='og:url' content='https://richkevan.com' />
         </Head>
     )
