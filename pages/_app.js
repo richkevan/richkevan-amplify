737cbc405d1196ef67c0f376d67a71e2d17246ae
Rich Kevan <rskevan@me.com>
2022-01-15 00:12:31 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import '@styles/globals.css'
 import Layout from '../components/layout'
 import PlausibleProvider from 'next-plausible';
-import * as Fathom from 'fathom-client'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBriefcase, faFileAlt, faEnvelope, faBlog, faHome } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -11,23 +8,6 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 library.add(faBriefcase, faFileAlt, faEnvelope, faBlog, faHome);
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    Fathom.load(XXMTEHMX,{
-      includedDomains: ['richkevan.com']
-    })
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, [])
 
   
   return (
