a46b3fdeff571ccac5f095cc0d63bc0709aa28fa
rich kevan <rskevan@me.com>
2022-01-18 20:32:48 -0800

    added missing dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '@styles/globals.css'
 import Layout from '../components/layout'
 import PlausibleProvider from 'next-plausible';
